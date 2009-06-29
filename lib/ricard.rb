e04a63118d0b2d38eb87514a169c86a8d57bdb68
Matt Aimonetti <mattaimonetti@gmail.com>
2009-06-29 01:19:42 -0700

    improved the client/server

--- lib/ricard.rb
+++ lib/ricard.rb
@@ -14,7 +14,7 @@ module Ricard
     def initialize(options={})
       default_options = {
         :timeout => 5, 
-        :port => 4280
+        :port => 9280
       }
       @options = default_options.merge(options)
     end
@@ -30,14 +30,14 @@ module Ricard
       tr = DNSSD::TextRecord.new
       tr['description'] = "Ricard server for Pastis"
       
-      DNSSD.register('ricard', '_ricard._tcp', 'local', options[:port], tr.encode) do |rr|
+      DNSSD.register('ricard', '_ricard._tcp', '', options[:port], tr.encode) do |rr|
         puts "Ricard server announced to run on port #{options[:port]}"
       end
     end 
     
     def start_server
       EventMachine::run {
-        EventMachine::start_server "127.0.0.1", options[:port], RicardEventServer
+        EventMachine::start_server "0.0.0.0", options[:port], RicardEventServer
         puts "Listening for requests on #{options[:port]}"
       }
     end
@@ -47,18 +47,16 @@ module Ricard
 end 
 
 module RicardEventServer
-  def post_init 
-    @data = ''
-  end
   
   def receive_data(data)
-    @data << data
     p "received data #{data}"
-    if @data.scan(/;/).size == 2
+    if data.scan(/;/).size == 2
       # B64 Encoded COMMAND;PLUGIN;
       command, plugin = data.split(';').map {|s| Base64.decode64(s) }
       # do something with the query
       $stderr.puts "Plugin: #{plugin}, Command: #{command}"
+      send_data("Plugin: #{plugin}, Command: #{command}")
     end
   end
+  
 end
--- lib/ricard_client.rb
+++ lib/ricard_client.rb
@@ -3,7 +3,10 @@ require 'base64'
 
 module Ricard 
   class Client 
-    attr_reader :options
+    attr_reader :options, :server_info
+    
+    class DiscoveryError < StandardError; end
+    ServerInfo = Struct.new(:ip, :port, :domain)
     
     def initialize(options={})
       default_options = {
@@ -11,33 +14,37 @@ module Ricard
       }
       @options = default_options.merge(options)
     end
+     
+    # Returns an array with the server ip, port and domain
+    def discover_ricard_server
+      return server_info if server_info
+      server_discovery = DNSSD.resolve('ricard', '_ricard._tcp', 'local') do |r|
+        route_results = `traceroute #{r.target}` 
+        # looking up the ip since calling the server using the domain won't work :(
+        ip = route_results[/\((.*)\)/,1] 
+        raise ::Ricard::Client::DiscoveryError, "The ricard server wasn't found on the LAN" unless ip
+        @server_info = ServerInfo.new(ip, r.port, r.target)
+        puts "Found the Ricard service at #{@server_info.domain} #{@server_info.port} on ip: #{@server.ip}"
+      end
+      sleep options[:timeout]
+      server_discovery.stop
+      raise ::Ricard::Client::DiscoveryError, "No Ricard server found on the LAN, make sure you started a server" unless server_info
+      server_info
+    end
     
     def send_command(command, plugin='ricard')
       puts "Sending: '#{command}' to the #{plugin} Ricard Server plugin"
-      
-      # DNSSD.resolve()
-      
-      # service = DNSSD.browse('_ricard._tcp') do |reply|
-      #   server = reply
-      #   data = [plugin, command].map do |s| 
-      #     Base64.encode64(s)
-      #   end.join(';') + ";"
-      #   TCPSocket.new(r.target, r.port).send(data, 0)
-      # end        
-      # 
-      # STDERR.puts "looking for the Ricard server (waiting 2 secs)"
-      # service.stop
-            
-      server_discovery = DNSSD.resolve('ricard', '_ricard._tcp', 'local') do |r|
-        puts "Found the Ricard service at #{r.target} #{r.port}"
-        data = [plugin, command].map do |s| 
-          Base64.encode64(s)
-        end.join(';') + ";"
-        TCPSocket.new(r.target, r.port).send(data, 0)
-      end  
-      
-      sleep options[:timeout]
-      server_discovery.stop
+      ricard_server = discover_ricard_server
+
+      data = [plugin, command].map do |s| 
+        Base64.encode64(s)
+      end.join(';') + ";"
+
+      ricard = TCPSocket.new(ricard_server.ip, ricard_server.port)  #.send(data, 0)
+      ricard.send(data, 0)
+      puts ricard.recv(100)
+      ricard.close    
     end
+    
   end
 end
